ee1136c1902f61ae7abf8fad08b3e603cf4e300e
Branine <146031197+Branine@users.noreply.github.com>
2024-04-10 14:32:40 -0400

    Update characterDB.sql

--- characterDB.sql
+++ characterDB.sql
@@ -1,6 +1,8 @@
-/* characterDB.sql */
+/* characterDB.sql i have no idea if im doing this right -J */
 CREATE TABLE character (
   characterID NUMERIC(10) NOT NULL,
-  
-
+  raceCode varchar(50) DEFAULT NULL,
+  classCode varchar(50) DEFAULT NULL,
+  genderCode varchar(50) DEFAULT NULL,  
+  PRIMARY KEY (characterID)
   );
