a2216b514198fb062f373ad19c121fc701018807
Branine <146031197+Branine@users.noreply.github.com>
2024-04-11 12:01:15 -0400

    Update characterDB.sql

--- characterDB.sql
+++ characterDB.sql
@@ -10,12 +10,12 @@ CREATE TABLE characters (
 CREATE TABLE abilities (
   /*abilityID INT NOT NULL,*/
   abilityID INTEGER PRIMARY KEY AUTO_INCREMENT,
-  strength INT NOT NULL,
-  dexterity INT NOT NULL,
-  constitution INT NOT NULL,
-  intelligence INT NOT NULL,
-  wisdom INT NOT NULL,
-  charisma INT NOT NULL,
+  strength INTEGER NOT NULL,
+  dexterity INTEGER NOT NULL,
+  constitution INTEGER NOT NULL,
+  intelligence INTEGER NOT NULL,
+  wisdom INTEGER NOT NULL,
+  charisma INTEGER NOT NULL,
  /* PRIMARY KEY(abilityID),*/
  /* characterID INTEGER,*/ /*foreign keys need to be defined in the table*/
   FOREIGN KEY (characterID) REFERENCES characters(characterID)
@@ -23,24 +23,24 @@ CREATE TABLE abilities (
 
 CREATE TABLE skills (
   skillID INTEGER PRIMARY KEY AUTO_INCREMENT, /*added primary key for skills*/
-  acrobatics INT NOT NULL,
-  animalHandling INT NOT NULL,
-  arcana INT NOT NULL,
-  athletics INT NOT NULL,
-  deception INT NOT NULL,
-  history INT NOT NULL,
-  insight INT NOT NULL,
-  intimidation INT NOT NULL,
-  investigation INT NOT NULL,
-  medicine INT NOT NULL,
-  nature INT NOT NULL,
-  perception INT NOT NULL,
-  performance INT NOT NULL,
-  persuasion INT NOT NULL,
-  religion INT NOT NULL,
-  sleightOfHand INT NOT NULL,
-  stealth INT NOT NULL,
-  survival INT NOT NULL,
+  acrobatics INTEGER NOT NULL,
+  animalHandling INTEGER NOT NULL,
+  arcana INTEGER NOT NULL,
+  athletics INTEGER NOT NULL,
+  deception INTEGER NOT NULL,
+  history INTEGER NOT NULL,
+  insight INTEGER NOT NULL,
+  intimidation INTEGER NOT NULL,
+  investigation INTEGER NOT NULL,
+  medicine INTEGER NOT NULL,
+  nature INTEGER NOT NULL,
+  perception INTEGER NOT NULL,
+  performance INTEGER NOT NULL,
+  persuasion INTEGER NOT NULL,
+  religion INTEGER NOT NULL,
+  sleightOfHand INTEGER NOT NULL,
+  stealth INTEGER NOT NULL,
+  survival INTEGER NOT NULL,
   /*characterID INTEGER,*/ /*foreign keys need to be defined in the table*/
   FOREIGN KEY (characterID) REFERENCES characters(characterID)
 );
