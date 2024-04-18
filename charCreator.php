e8b0290efff8dc8f33526ddeb7e7181a1ce2b527
Sam Zito <101850483+SamNZito@users.noreply.github.com>
2024-04-18 12:47:54 -0400

    Update charCreator.php
    added skills to the creation form
    added a skills table
    updated some var names

--- charCreator.php
+++ charCreator.php
@@ -5,15 +5,22 @@ $query = 'SELECT * FROM characters';
 $statement = $db->prepare($query);
 //$statement->bindValue(':character_id', $character_id);
 $statement->execute();
-$products = $statement->fetchAll();
+$chars = $statement->fetchAll();
 $statement->closeCursor();
 
 $query2 = 'SELECT * FROM abilities';
 $statement2 = $db->prepare($query2);
 //$statement->bindValue(':character_id', $character_id);
 $statement2->execute();
-$products2 = $statement2->fetchAll();
+$abilities = $statement2->fetchAll();
 $statement2->closeCursor();
+
+
+$query3 = 'SELECT * FROM skills';
+$statement3 = $db->prepare($query3);
+$statement3->execute();
+$skills = $statement3->fetchAll();
+$statement3->closeCursor();
 
 ?>
 
@@ -39,24 +46,83 @@ $statement2->closeCursor();
         <label for="gender">Gender:</label><br>
         <input type="text" id="gender" name="gender" required><br><br>
 
+<!--=====================ABILITY============================================ -->
+
         <h2>Abilities</h2>
         <label for="strength">Strength:</label><br>
-        <input type="text" id="strength" name="strength" required><br><br>
+        <input type="number" id="strength" name="strength" min="0" max="20" required><br><br>
 
         <label for="dexterity">Dexterity:</label><br>
-        <input type="text" id="dexterity" name="dexterity" required><br><br>
+        <input type="number" id="dexterity" name="dexterity" min="0" max="20" required><br><br>
 
         <label for="constitution">Constitution:</label><br>
-        <input type="text" id="constitution" name="constitution" required><br><br>
+        <input type="number" id="constitution" name="constitution" min="0" max="20" required><br><br>
 
         <label for="intelligence">Intelligence:</label><br>
-        <input type="text" id="intelligence" name="intelligence" required><br><br>
+        <input type="number" id="intelligence" name="intelligence" min="0" max="20" required><br><br>
 
         <label for="wisdom">Wisdom:</label><br>
-        <input type="text" id="wisdom" name="wisdom" required><br><br>
+        <input type="number" id="wisdom" name="wisdom" min="0" max="20" required><br><br>
 
         <label for="charisma">Charisma:</label><br>
-        <input type="text" id="charisma" name="charisma" required><br><br>
+        <input type="number" id="charisma" name="charisma" min="0" max="20" required><br><br>
+
+<!-- ========================SKILLS==============================================-->
+
+        <h2>Skills</h2>
+    <label for="acrobatics">Acrobatics:</label><br>
+    <input type="number" id="acrobatics" name="acrobatics" min="0" value="0"><br><br>
+
+    <label for="animalHandling">Animal Handling:</label><br>
+    <input type="number" id="animalHandling" name="animalHandling" min="0" value="0"><br><br>
+
+    <label for="arcana">Arcana:</label><br>
+    <input type="number" id="arcana" name="arcana" min="0" value="0"><br><br>
+
+    <label for="athletics">Athletics:</label><br>
+    <input type="number" id="athletics" name="athletics" min="0"  value="0"><br><br>
+
+    <label for="deception">Deception:</label><br>
+    <input type="number" id="deception" name="deception" min="0" value="0"><br><br>
+
+    <label for="history">History:</label><br>
+    <input type="number" id="history" name="history" min="0" value="0"><br><br>
+
+    <label for="insight">Insight:</label><br>
+    <input type="number" id="insight" name="insight" min="0" value="0"><br><br>
+
+    <label for="intimidation">Intimidation:</label><br>
+    <input type="number" id="intimidation" name="intimidation" min="0" value="0"><br><br>
+
+    <label for="investigation">Investigation:</label><br>
+    <input type="number" id="investigation" name="investigation" min="0" value="0"><br><br>
+
+    <label for="medicine">Medicine:</label><br>
+    <input type="number" id="medicine" name="medicine" min="0" value="0"><br><br>
+
+    <label for="nature">Nature:</label><br>
+    <input type="number" id="nature" name="nature" min="0" value="0"><br><br>
+
+    <label for="perception">Perception:</label><br>
+    <input type="number" id="perception" name="perception" min="0" value="0"><br><br>
+
+    <label for="performance">Performance:</label><br>
+    <input type="number" id="performance" name="performance" min="0" value="0"><br><br>
+
+    <label for="persuasion">Persuasion:</label><br>
+    <input type="number" id="persuasion" name="persuasion" min="0" value="0"><br><br>
+
+    <label for="religion">Religion:</label><br>
+    <input type="number" id="religion" name="religion" min="0" value="0"><br><br>
+
+    <label for="sleightOfHand">Sleight of Hand:</label><br>
+    <input type="number" id="sleightOfHand" name="sleightOfHand" min="0" value="0"><br><br>
+
+    <label for="stealth">Stealth:</label><br>
+    <input type="number" id="stealth" name="stealth" min="0" value="0"><br><br>
+
+    <label for="survival">Survival:</label><br>
+    <input type="number" id="survival" name="survival" min="0" value="0"><br><br>
 
         <input type="submit" value="Submit">
       </form>
@@ -72,19 +138,18 @@ $statement2->closeCursor();
       </tr>
     </thead>
     <tbody>
-  <?php foreach ($products as $product) : ?>
+  <?php foreach ($chars as $char) : ?>
   <tr>
-     <td><?php echo $product['characterID']; ?></td>
-     <td><?php echo $product['race']; ?></td>
-     <td><?php echo $product['class']; ?></td>
-     <td><?php echo $product['gender']; ?></td>
+     <td><?php echo $char['characterID']; ?></td>
+     <td><?php echo $char['race']; ?></td>
+     <td><?php echo $char['class']; ?></td>
+     <td><?php echo $char['gender']; ?></td>
   </tr>
   <?php endforeach; ?>
   </tbody>
   </table>
  
 <h2>------------------------------------</h2> 
-
 <h1>Abilities Table</h1>
 
   <table>
@@ -101,22 +166,78 @@ $statement2->closeCursor();
       </tr>
     </thead>
     <tbody>
-  <?php foreach ($products2 as $product2) : ?>
+  <?php foreach ($abilities as $ability) : ?>
   <tr>
-     <td><?php echo $product2['abilityID']; ?></td>
-     <td><?php echo $product2['strength']; ?></td>
-     <td><?php echo $product2['dexterity']; ?></td>
-     <td><?php echo $product2['constitution']; ?></td>
-     <td><?php echo $product2['intelligence']; ?></td>
-     <td><?php echo $product2['wisdom']; ?></td>
-     <td><?php echo $product2['charisma']; ?></td>
-     <td><?php echo $product2['characterID']; ?></td>
+     <td><?php echo $ability['abilityID']; ?></td>
+     <td><?php echo $ability['strength']; ?></td>
+     <td><?php echo $ability['dexterity']; ?></td>
+     <td><?php echo $ability['constitution']; ?></td>
+     <td><?php echo $ability['intelligence']; ?></td>
+     <td><?php echo $ability['wisdom']; ?></td>
+     <td><?php echo $ability['charisma']; ?></td>
+     <td><?php echo $ability['characterID']; ?></td>
   </tr>
   <?php endforeach; ?>
   </tbody>
   </table>
-  <footer>Copyright &copy; 2024 Alec Manzer, Sam Zito, 
-  Fernando Moran, Janine Beall, Kennedy Greene</footer>
+
+<h2>--------------------------------------------------</h2>
+
+<h1>Skills Table</h1>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Acrobatics</th>
+            <th>Animal Handling</th>
+            <th>Arcana</th>
+            <th>Athletics</th>
+            <th>Deception</th>
+            <th>History</th>
+            <th>Insight</th>
+            <th>Intimidation</th>
+            <th>Investigation</th>
+            <th>Medicine</th>
+            <th>Nature</th>
+            <th>Perception</th>
+            <th>Performance</th>
+            <th>Persuasion</th>
+            <th>Religion</th>
+            <th>Sleight of Hand</th>
+            <th>Stealth</th>
+            <th>Survival</th>
+            <th>Character ID</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($skills as $skill) : ?>
+            <tr>
+                <td><?php echo $skill['skillID']; ?></td>
+                <td><?php echo $skill['acrobatics']; ?></td>
+                <td><?php echo $skill['animalHandling']; ?></td>
+                <td><?php echo $skill['arcana']; ?></td>
+                <td><?php echo $skill['athletics']; ?></td>
+                <td><?php echo $skill['deception']; ?></td>
+                <td><?php echo $skill['history']; ?></td>
+                <td><?php echo $skill['insight']; ?></td>
+                <td><?php echo $skill['intimidation']; ?></td>
+                <td><?php echo $skill['investigation']; ?></td>
+                <td><?php echo $skill['medicine']; ?></td>
+                <td><?php echo $skill['nature']; ?></td>
+                <td><?php echo $skill['perception']; ?></td>
+                <td><?php echo $skill['performance']; ?></td>
+                <td><?php echo $skill['persuasion']; ?></td>
+                <td><?php echo $skill['religion']; ?></td>
+                <td><?php echo $skill['sleightOfHand']; ?></td>
+                <td><?php echo $skill['stealth']; ?></td>
+                <td><?php echo $skill['survival']; ?></td>
+                <td><?php echo $skill['characterID']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
 </body>
 
 </html>
