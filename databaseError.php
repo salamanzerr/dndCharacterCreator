aa9943e1f961cd650a2b494b3ead77e5e4b95808
Sam Zito <101850483+SamNZito@users.noreply.github.com>
2024-04-22 12:01:13 -0400

    Create databaseError.php
    added databaseError.php

---
+++ databaseError.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <!-- the head section -->
+    <head>
+        <title>DND Character Creator</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+
+    <!-- the body section -->
+    <body>
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed as described in the appendix.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+    </main>
+    </body>
+</html>
