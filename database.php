be524f5228efd54b123257a40c9e350da0acf104
Sam Zito <101850483+SamNZito@users.noreply.github.com>
2024-04-26 17:42:12 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-//$dsn='mysql:host=joecool.highpoint.edu;dbname=CSC3212_S24_YOURNAME_db';
-//$username=YOURUSERNAME;
-//$password=YOURPASSWORD;
+$dsn='mysql:host=joecool.highpoint.edu;dbname=CSC3212_S24_YOURNAME_db';
+$username=YOURUSERNAME;
+$password=YOURPASSWORD;
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
